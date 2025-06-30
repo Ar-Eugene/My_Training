a63455b6ab7a0d55d6d7128be4844ba123464248
User <artemenkoe2711@gmail.com>
2025-06-30 18:17:35 +0400

    Пишем экран профиля. Тему и цвета снова в core перенес

--- feature-profile/src/main/java/com/example/feature_profile/presentation/ui/ProfileScreen.kt
+++ feature-profile/src/main/java/com/example/feature_profile/presentation/ui/ProfileScreen.kt
@@ -1,18 +1,51 @@
 package com.example.feature_profile.presentation.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.core.ui.theme.MyTrainingTheme
+import com.example.feature_profile.R
 
 @Composable
 fun ProfileScreen(modifier: Modifier = Modifier) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .padding(top = dimensionResource(R.dimen.padding_24dp)),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Profile Screen")
+        Image(
+            contentDescription = null,
+            modifier = Modifier
+                .size(dimensionResource(R.dimen.image_size))
+                .padding(dimensionResource(R.dimen.padding_8dp)),
+            painter = painterResource(R.drawable.study_test)
+        )
+        Text(
+            text = stringResource(R.string.user)
+        )
+    }
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ProfileScreenPreview() {
+    MyTrainingTheme {
+        ProfileScreen(modifier = Modifier)
     }
 }
