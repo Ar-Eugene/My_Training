5f06693421855ff77fdcfd6019cd39c64d1dac8b
User <artemenkoe2711@gmail.com>
2025-08-26 20:57:33 +0400

    Поправил расположение кнопок в HomeScreen. Начал наполнять экран OgeScreen.

--- app/src/main/java/com/example/mytraining/precentation/ui/HomeScreen.kt
+++ app/src/main/java/com/example/mytraining/precentation/ui/HomeScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -25,10 +27,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.core.domain.models.ExamType
-import com.example.core.ui.theme.BackgroundGradientGreen
 import com.example.core.ui.theme.BackgroundGradientBlue
+import com.example.core.ui.theme.BackgroundGradientGreen
+import com.example.core.ui.theme.BottomNavigationColor
+import com.example.core.ui.theme.CardBackgroundGradientBrown
 import com.example.feature_ege.presentation.ui.EgeScreen
 import com.example.feature_oge.presentation.ui.OgeScreen
 import com.example.mytraining.precentation.viewmodel.HomeViewModel
@@ -47,15 +53,18 @@ fun HomeScreen(homeViewModel: HomeViewModel = hiltViewModel()) {
         modifier = Modifier
             .fillMaxSize()
             .background(brush = Brush.linearGradient(colors = backgroundGradientColor))
+            .statusBarsPadding()
     ) {
-        // --- Кнопки выбора типа экзамена ---
+        // Кнопки выбора типа экзамена
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(dimensionResource(com.example.core.R.dimen.padding_16dp)),
-            horizontalArrangement = Arrangement.Start
+            horizontalArrangement = Arrangement.Center
         ) {
             SelectExamButton(
+                modifier = Modifier
+                    .weight(1f),
                 text = "ОГЭ",
                 selected = examTypeState.value == ExamType.OGE,
                 onClick = {
@@ -65,6 +74,8 @@ fun HomeScreen(homeViewModel: HomeViewModel = hiltViewModel()) {
             )
             Spacer(modifier = Modifier.width(dimensionResource(com.example.core.R.dimen.padding_8dp)))
             SelectExamButton(
+                modifier = Modifier
+                    .weight(1f),
                 text = "ЕГЭ",
                 selected = examTypeState.value == ExamType.EGE,
                 onClick = {
@@ -88,18 +99,30 @@ fun HomeScreen(homeViewModel: HomeViewModel = hiltViewModel()) {
 
 @Composable
 fun SelectExamButton(
+    modifier: Modifier = Modifier,
     text: String,
     selected: Boolean,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     Button(
+        modifier = modifier
+            .height(80.dp),
         onClick = onClick,
         colors = ButtonDefaults.buttonColors(
-            containerColor = if (selected) MaterialTheme.colorScheme.primary else Color.LightGray,
+            containerColor = if (selected) CardBackgroundGradientBrown else BottomNavigationColor,
             contentColor = if (selected) Color.White else Color.Black
         ),
-        shape = RoundedCornerShape(dimensionResource(com.example.core.R.dimen.padding_8dp))
+        shape = RoundedCornerShape(dimensionResource(com.example.core.R.dimen.padding_8dp)),
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 8.dp,
+            pressedElevation = 4.dp,
+            disabledElevation = 0.dp
+        )
     ) {
-        Text(text)
+        Text(
+            text, style = MaterialTheme.typography.displayMedium,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
--- feature-oge/src/main/java/com/example/feature_oge/presentation/ui/OgeScreen.kt
+++ feature-oge/src/main/java/com/example/feature_oge/presentation/ui/OgeScreen.kt
@@ -1,18 +1,105 @@
 package com.example.feature_oge.presentation.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.example.core.ui.theme.BottomNavigationColor
 
 @Composable
 fun OgeScreen(modifier: Modifier = Modifier) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(horizontal = 16.dp) // Добавляем горизонтальные отступы
     ) {
-        Text("Oge Screen")
+        // Заголовок
+        Text(
+            text = "Выберите предмет",
+            style = MaterialTheme.typography.headlineMedium,
+            color = Color.White,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+
+        // Список предметов
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2), // 2 колонки
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalArrangement = Arrangement.spacedBy(6.dp),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(ogeSubjects) { subject ->
+                SelectSubject(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = subject,
+                    onClick = {
+                        println("Выбран предмет: $subject")
+                    }
+                )
+            }
+        }
+    }
+}
+
+// Список предметов ОГЭ для демонстрации
+val ogeSubjects = listOf(
+    "Русский язык",
+    "Английский язык",
+    "Математика",
+    "Литература",
+    "Русский язык",
+    "Английский язык",
+    "Математика",
+    "Литература"
+)
+@Composable
+fun SelectSubject(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier
+            .height(80.dp),
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = BottomNavigationColor.copy(alpha = 0.9f),
+            contentColor = Color(0xFF324379)
+        ),
+        shape = RoundedCornerShape(dimensionResource(com.example.core.R.dimen.padding_8dp)),
+
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelSmall,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+        )
     }
 }
